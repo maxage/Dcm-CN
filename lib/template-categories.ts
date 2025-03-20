a0274b0101a7b5d07711fbe4eb838f716f310d5f
Thomas Camlong <thomas@ajnart.fr>
2025-03-21 00:29:17 +0100

    refactor: move template definitions to separate files and fix circular dependency

---
+++ lib/template-categories.ts
@@ -0,0 +1,10 @@
+// Template categories
+export enum TemplateCategory {
+  MEDIA = "Media",
+  DEVELOPMENT = "Development",
+  MONITORING = "Monitoring",
+  DATABASE = "Database",
+  PRODUCTIVITY = "Productivity",
+  SECURITY = "Security",
+  OTHER = "Other",
+} 
